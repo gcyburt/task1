4482306ae0e4c7aedeaba6caeeefb393cdea7669
gcyburt <gcyburt@gmail.com>
2022-05-15 18:00:37 +0200

    easy to read table done

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import json
+from prettytable import PrettyTable
 
 # geting JSON file from the provided page and storing whole struct in sellersDeserialized
 with open("sellers.json", "r") as read_file:
     sellersDeserialized = json.load(read_file)
+
+outputFile = open('results.txt', 'w', encoding="utf-8")
+
+table = PrettyTable(['ID', 'Name', 'Domain', 'Seller type', 'Direct domain?'])
+
+for seller in sellersDeserialized['sellers']:
+    if seller['seller_type'] in ['INTERMEDIARY', 'BOTH']:
+        if 'via' in seller['name']:
+            isDirect = "indirect"
+        else:
+            isDirect = "direct"
+        table.add_row([seller['seller_id'], seller['name'], seller['domain'], seller['seller_type'], isDirect])
+
+outputFile.write(str(table))
+print(table)
+outputFile.close()
